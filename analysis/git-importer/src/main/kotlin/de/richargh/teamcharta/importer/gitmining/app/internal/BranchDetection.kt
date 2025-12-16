4733a0d8a59c932806f09344cc337cb970957e1e
Richard Gross <richard.gross@maibornwolff.de>
2025-12-16 15:42:52 +0100

    . r flip && conditions

--- analysis/git-importer/src/main/kotlin/de/richargh/teamcharta/importer/gitmining/app/internal/BranchDetection.kt
+++ analysis/git-importer/src/main/kotlin/de/richargh/teamcharta/importer/gitmining/app/internal/BranchDetection.kt
@@ -59,7 +59,7 @@ private class BranchCollector(private val commits: List<Commit>) {
         }
     }
 
-    private fun isMergeOnActiveBranch(commit: Commit): Boolean = (!hasActiveBranch || commit.isOnActiveBranch) && commit.isMergeCommit
+    private fun isMergeOnActiveBranch(commit: Commit): Boolean = commit.isMergeCommit && (!hasActiveBranch || commit.isOnActiveBranch)
 
     private fun recordNamedMerge(
         mergeCommit: Commit,
