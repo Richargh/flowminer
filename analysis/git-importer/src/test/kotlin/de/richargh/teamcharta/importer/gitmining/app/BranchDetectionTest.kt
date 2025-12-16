39ddb928c14874b03dc2c79159ee24174490a182
Richard Gross <richard.gross@maibornwolff.de>
2025-12-16 13:24:36 +0100

    . t add tests for inferred and unnamed branches

--- analysis/git-importer/src/test/kotlin/de/richargh/teamcharta/importer/gitmining/app/BranchDetectionTest.kt
+++ analysis/git-importer/src/test/kotlin/de/richargh/teamcharta/importer/gitmining/app/BranchDetectionTest.kt
@@ -426,5 +426,192 @@ class BranchDetectionTest {
                 mergedInto("origin/trunk", "2025-01-01T06:00:00+01:00".zoned(), "6")
             }
         }
+
+        @Test
+        fun `should identify inferred branches`() {
+            // trunk: 0───1───────4───────7 (HEAD -> trunk, origin/trunk)
+            //         \         / \     /
+            // feat-a:  └─2───3─┘   \   / (deleted, inferred from merge message)
+            //                       \ /
+            // feat-b:                5─6 (deleted, inferred from merge message)
+
+            // Given
+            val gitLogContent = """
+            -----COMMIT_START-----
+            HEAD -> trunk, origin/trunk, origin/HEAD|7|4 6|2025-01-01T07:00:00+01:00|John Doe|john@example.com|Merge branch 'origin/feat-b' into origin/trunk
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+
+            -----COMMIT_START-----
+            |6|5|2025-01-01T06:00:00+01:00|John Doe|john@example.com|feat-b 2 commit
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+            0       0       feat-b2.md
+
+            -----COMMIT_START-----
+            |5|1|2025-01-01T05:00:00+01:00|John Doe|john@example.com|feat-b 1 commit
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+            0       0       feat-b1.md
+
+            -----COMMIT_START-----
+            |4|1 3|2025-01-01T04:00:00+01:00|John Doe|john@example.com|Merge branch 'origin/feat-a' into origin/trunk
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+
+            -----COMMIT_START-----
+            |3|2|2025-01-01T03:00:00+01:00|John Doe|john@example.com|feat-a 2 commit
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+            0       0       feat-a2.md
+
+            -----COMMIT_START-----
+            |2|0|2025-01-01T02:00:00+01:00|John Doe|john@example.com|feat-a 1 commit
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+            0       0       feat-a1.md
+
+            -----COMMIT_START-----
+            |1|0|2025-01-01T01:00:00+01:00|John Doe|john@example.com|main commit
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+            0       0       trunk2.md
+
+            -----COMMIT_START-----
+            |0||2025-01-01T00:00:00+01:00|John Doe|john@example.com|initial commit
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+            0       0       trunk.md
+        """.trimIndent()
+
+            val testee = GitLogMining()
+
+            // When
+            val result = testee.parse(gitLogContent.lineSequence())
+
+            // Then
+            result.branches.size() shouldBe 3
+            result.branches["origin/trunk"] shouldBe aBranch {
+                name("origin/trunk")
+                firstCommitHash("0".hash())
+                firstCommitDate("2025-01-01T00:00:00+01:00".zoned())
+            }
+            result.branches["origin/feat-a"] shouldBe aBranch {
+                inferredName("origin/feat-a")
+                firstCommitHash("2".hash())
+                firstCommitDate("2025-01-01T02:00:00+01:00".zoned())
+                mergedInto("origin/trunk", "2025-01-01T04:00:00+01:00".zoned(), "4")
+            }
+            result.branches["origin/feat-b"] shouldBe aBranch {
+                inferredName("origin/feat-b")
+                firstCommitHash("5".hash())
+                firstCommitDate("2025-01-01T05:00:00+01:00".zoned())
+                mergedInto("origin/trunk", "2025-01-01T07:00:00+01:00".zoned(), "7")
+            }
+        }
+
+        @Test
+        fun `should identify unnamed branches`() {
+            // trunk: 0───1───────4───────7 (HEAD -> trunk, origin/trunk)
+            //         \         / \     /
+            // ???:     └─2───3─┘   \   / (deleted, cannot infer name)
+            //                       \ /
+            // ???:                   5─6 (deleted, cannot infer name)
+
+            // Given
+            val gitLogContent = """
+            -----COMMIT_START-----
+            HEAD -> trunk, origin/trunk, origin/HEAD|7|4 6|2025-01-01T07:00:00+01:00|John Doe|john@example.com|Squashed second feature
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+
+            -----COMMIT_START-----
+            |6|5|2025-01-01T06:00:00+01:00|John Doe|john@example.com|feat-b 2 commit
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+            0       0       feat-b2.md
+
+            -----COMMIT_START-----
+            |5|1|2025-01-01T05:00:00+01:00|John Doe|john@example.com|feat-b 1 commit
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+            0       0       feat-b1.md
+
+            -----COMMIT_START-----
+            |4|1 3|2025-01-01T04:00:00+01:00|John Doe|john@example.com|Squashed first feature
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+
+            -----COMMIT_START-----
+            |3|2|2025-01-01T03:00:00+01:00|John Doe|john@example.com|feat-a 2 commit
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+            0       0       feat-a2.md
+
+            -----COMMIT_START-----
+            |2|0|2025-01-01T02:00:00+01:00|John Doe|john@example.com|feat-a 1 commit
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+            0       0       feat-a1.md
+
+            -----COMMIT_START-----
+            |1|0|2025-01-01T01:00:00+01:00|John Doe|john@example.com|main commit
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+            0       0       trunk2.md
+
+            -----COMMIT_START-----
+            |0||2025-01-01T00:00:00+01:00|John Doe|john@example.com|initial commit
+            -----BODY_START-----
+            -----TRAILERS_START-----
+            -----FILES_START-----
+            0       0       trunk.md
+        """.trimIndent()
+
+            val testee = GitLogMining()
+
+            // When
+            val result = testee.parse(gitLogContent.lineSequence())
+
+            // Then
+            result.branches.size() shouldBe 3
+            result.branches["origin/trunk"] shouldBe aBranch {
+                name("origin/trunk")
+                firstCommitHash("0".hash())
+                firstCommitDate("2025-01-01T00:00:00+01:00".zoned())
+            }
+            result.branches.unnamed shouldHaveSize 2
+            result.branches.unnamed.any { branch ->
+                branch == aBranch {
+                    unNamed()
+                    firstCommitHash("2".hash())
+                    firstCommitDate("2025-01-01T02:00:00+01:00".zoned())
+                    mergedInto("origin/trunk", "2025-01-01T04:00:00+01:00".zoned(), "4")
+                }
+            } shouldBe true
+            result.branches.unnamed.any { branch ->
+                branch == aBranch {
+                    unNamed()
+                    firstCommitHash("5".hash())
+                    firstCommitDate("2025-01-01T05:00:00+01:00".zoned())
+                    mergedInto("origin/trunk", "2025-01-01T07:00:00+01:00".zoned(), "7")
+                }
+            } shouldBe true
+        }
     }
 }
