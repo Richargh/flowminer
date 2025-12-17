8e7a34fe9a276ec1bed7e9da13fe5a2095a4c6a5
Richard Gross <richard.gross@maibornwolff.de>
2025-12-17 23:07:27 +0100

    . r (work items) identify absolute churn by author

--- analysis/git-importer/src/main/kotlin/de/richargh/teamcharta/importer/gitmining/app/internal/WorkItemMiner.kt
+++ analysis/git-importer/src/main/kotlin/de/richargh/teamcharta/importer/gitmining/app/internal/WorkItemMiner.kt
@@ -33,7 +33,7 @@ private class MutableWorkKey(
     private var linesRemoved = 0
     private var lastCommitDate: ZonedDateTime = firstCommitDate
     private val filesChanged = mutableSetOf<Path>()
-    private val authorLines = mutableMapOf<Author, Int>()
+    private val absoluteChurnByAuthor = mutableMapOf<Author, Int>()
     private val absoluteChurnByType = mutableMapOf<CommitType, Int>()
 
     fun add(commit: Commit) {
@@ -45,7 +45,7 @@ private class MutableWorkKey(
             commitLinesChanged += fileChange.additions + fileChange.deletions
         }
 
-        authorLines[commit.author] = authorLines.getOrDefault(commit.author, 0) + commitLinesChanged
+        absoluteChurnByAuthor[commit.author] = absoluteChurnByAuthor.getOrDefault(commit.author, 0) + commitLinesChanged
         absoluteChurnByType[commit.commitType] = absoluteChurnByType.getOrDefault(commit.commitType, 0) + commitLinesChanged
 
         val commitDate = commit.date
@@ -64,7 +64,7 @@ private class MutableWorkKey(
         firstCommitDate = firstCommitDate,
         lastCommitDate = lastCommitDate,
         filesChanged = filesChanged.toSet(),
-        contributions = authorLines
+        contributions = absoluteChurnByAuthor
             .map { (author, lines) -> AuthorContribution(author, lines) }
             .sortedByDescending { it.linesChanged },
         absoluteChurnByType = absoluteChurnByType.toMap()
